0a8268edc077b48dd7e27f8aee53063dc945cd5b
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-10-13 12:24:37 +0530

    cell reveal update

--- Minesweeper/src/components/Cell.jsx
+++ Minesweeper/src/components/Cell.jsx
@@ -1,7 +1,26 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
 import '../styles/cell.css'
 
 export default function Cell({showed, x, y, flagged, value, hasBomb}) {
+    const [clicked, setClicked] = useState(false);
+
+    const handleCellClick = () => {
+        setClicked(true);
+        showed = true;
+    }
     return (
-        <div className="cell"></div>
+        <div className="cell" onClick={handleCellClick}>
+            {showed || clicked ? hasBomb ? 'X' : value : null }
+        </div>
     )
+}
+
+Cell.propTypes = {
+    showed: PropTypes.bool,
+    x: PropTypes.number,
+    y: PropTypes.number,
+    flagged: PropTypes.bool,
+    value: PropTypes.number,
+    hasBomb: PropTypes.bool
 }
